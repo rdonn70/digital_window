3a050a43e877b3d511b038bb409338f72ffe976e
Ryan Donnellan <69125296+rdonn70@users.noreply.github.com>
2026-03-19 19:29:32 -0500

    Update OffAxisProjection.cs

--- source/OffAxisProjection.cs
+++ source/OffAxisProjection.cs
@@ -1,88 +1,42 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-// Heavily borrows from Robert Kooima's Generalized Perspective Projection (2009) Paper: https://discussions.unity.com/uploads/short-url/r7D1Sc8bPTsZhNnSXHTBDJM0XEw.pdf
+using UnityEngine.InputSystem.XR;
 
 public class OffAxisProjection : MonoBehaviour
 {
     private bool calibrated = false;
-    private Vector3[] calibration_points = new Vector3[3];
-    private int calibration_step = 0;
-    private float width = 0f;
-    private float height = 0f;
     private float near = 0.05f;
     private float far = 100f;
 
     private float offsetX = 0f; // optional x adjustment offset
     private float offsetY = 0f; // optional y adjustment offset
+    private float offsetZ = 0f;
 
     private Camera cam;
     private UdpClient udp;
 
     private bool keyboard_mode_toggle = false;
     private Vector3 raw_pos = new Vector3(0f, 0f, 0f);
-    private Vector3 raw_pos_prev = new Vector3(0f, 0f, 0f);
     private float timestamp = 0f;
     private float timestamp_prev = 0f;
-    private Vector3 vr, vu, vn;
-    private Vector3 pa, pb, pc;
+
+    private Vector3 Xs, Ys, Zs;
+    private Vector3 LL = new Vector3(-0.3f, 0f, 0.7f);
+    private Vector3 LR = new Vector3(0.3f, 0f, 0.7f);
+    private Vector3 UL = new Vector3(-0.3f, 0.335f, 0.7f);
+    private Vector3 UR = new Vector3(0.3f, 0.335f, 0.7f);
+    private Matrix4x4 M, RotMat;
 
     private Vector3 filtered_prev = new Vector3(0f, 0f, 0f); // y_t-1
     private float min_cutoff_freq = 5.0f; // fc_min, confirugable
     private float speed_coefficient = 0.02f; // beta, configurable
     private float sample_interval = 0f; // T_e
 
-    void Calibration()
-    {
-        cam.transform.position = new Vector3(0, 0, -1);
-        cam.transform.rotation = Quaternion.identity;
-        cam.ResetProjectionMatrix();
-
-        if (Mouse.current.leftButton.wasPressedThisFrame) {
-            Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
-            float t = -ray.origin.z / ray.direction.z;
-            Vector3 hit = ray.origin + ray.direction * t;
-
-            calibration_points[calibration_step] = hit;
-            calibration_step++;
-
-            if (calibration_step >= 3) {
-                pa = calibration_points[0]; // bottom left
-                pb = calibration_points[1]; // bottom right
-                pc = calibration_points[2]; // top left
-
-                width = Vector3.Distance(pa, pb);
-                height = Vector3.Distance(pa, pc);
-
-                calibrated = true;
-            }
-        }
-
-    }
-
-    void OnGUI()
-    {
-        GUIStyle style = new GUIStyle();
-        style.fontSize = 24;
-        style.normal.textColor = Color.yellow;
-        if (calibrated != false) { // debug stuff
-            GUI.Label(new Rect(10, 10, 500, 30), $"{timestamp:F4}", style);
-            GUI.Label(new Rect(10, 40, 500, 30), "RawPos: " + raw_pos.ToString("F3"), style);
-            GUI.Label(new Rect(10, 70, 500, 30), $"Width: {width:F4}  OffsetX: {offsetX:F4}  OffsetY: {offsetY:F4}", style);
-            GUI.Label(new Rect(10, 100, 500, 30), $"Beta: {speed_coefficient:F4}  fc_min: {min_cutoff_freq:F4}", style);
-        } else { // calibration stuff
-            if (calibration_step == 0) {
-                GUI.DrawTexture(new Rect(26, (Screen.height - 13), 26, 26), Texture2D.whiteTexture);
-            } else if (calibration_step == 1) {
-                GUI.DrawTexture(new Rect((Screen.width - 13), (Screen.height - 13), 26, 26), Texture2D.whiteTexture);
-            } else if (calibration_step == 2) {
-                GUI.DrawTexture(new Rect(13, 13, 26, 26), Texture2D.whiteTexture);
-            }
-        }
-    }
+    private LineRenderer calibration_line;
 
     void Start()
     {
@@ -90,13 +44,45 @@ public class OffAxisProjection : MonoBehaviour
         cam.nearClipPlane = near;
         cam.farClipPlane = far;
 
-        // Using the power of unity, I can skip the subtraction/cross product to determine the right, up, and forward vectors :)
-        vr = Vector3.right;
-        vu = Vector3.up;
-        vn = Vector3.forward;
+        cam.transform.position = new Vector3(0, 0, 0);
+        cam.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        cam.ResetProjectionMatrix();
+
+        calibration_line = gameObject.AddComponent<LineRenderer>();
+        calibration_line.positionCount = 5;
+        calibration_line.startWidth = 0.01f;
+        calibration_line.endWidth = 0.01f;
+        calibration_line.material = new Material(Shader.Find("Sprites/Default"));
+        calibration_line.startColor = Color.red;
+        calibration_line.endColor = Color.red;
+        calibration_line.useWorldSpace = true;
+
+        calibration_line.enabled = true;
 
         udp = new UdpClient(5005); // start listening for the data from the python program
         udp.Client.Blocking = false;
+    }
+
+    void Calibration()
+    {
+        calibration_line.SetPosition(0, LL); // bottom left
+        calibration_line.SetPosition(1, LR); // to bottom right
+        calibration_line.SetPosition(2, UR); // to top right
+        calibration_line.SetPosition(3, UL); // to top left
+        calibration_line.SetPosition(4, LL); // back to bottom left
+
+        cam.transform.position = new Vector3(offsetX, offsetY, offsetZ);
+    }
+
+    void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 24;
+        style.normal.textColor = Color.yellow;
+        GUI.Label(new Rect(10, 10, 500, 30), $"{timestamp:F4}", style);
+        GUI.Label(new Rect(10, 40, 500, 30), "RawPos: " + raw_pos.ToString("F3"), style);
+        GUI.Label(new Rect(10, 70, 500, 30), $"OffsetX: {offsetX:F4}  OffsetY: {offsetY:F4}  OffsetZ: {offsetZ:F4}", style);
+        GUI.Label(new Rect(10, 100, 500, 30), $"Beta: {speed_coefficient:F4}  fc_min: {min_cutoff_freq:F4}", style);
     }
 
     void ReceiveUDP()
@@ -111,7 +97,7 @@ public class OffAxisProjection : MonoBehaviour
             timestamp = parsed.time;
 
             if (timestamp_prev <= timestamp) { // skip the packet if the timestamp is out of order... granted, could do TCP but I want that juicy speed boost :)
-                raw_pos = new Vector3(-parsed.x / 1000f, -parsed.y / 1000f, parsed.z / 1000f); // correcting incoming data from the python program
+                raw_pos = new Vector3(-parsed.x / 1000f, -parsed.y / 1000f, -parsed.z / 1000f); // correcting incoming data from the python program
                 sample_interval = (timestamp - timestamp_prev) / 1000f;
                 timestamp_prev = timestamp;
                 ApplyOffAxis();
@@ -121,65 +107,46 @@ public class OffAxisProjection : MonoBehaviour
 
     void ApplyOffAxis()
     {
-        if(sample_interval <= 0f) {
+        if(sample_interval <= 0f) { //skips update if timestamps are out of order
             return;
         }
 
         // Implementation of the 1 euro filter, taken from https://github.com/MKSharaf/OneEuroFilterExplained/
-        Vector3 signal_speed = (raw_pos - raw_pos_prev) / sample_interval;
+        Vector3 signal_speed = (raw_pos - filtered_prev) / sample_interval;
         float cutoff_freq = min_cutoff_freq + (speed_coefficient * signal_speed.magnitude);
 
         // let k = tau / sample interval
         float k = (1 / (2 * Mathf.PI * cutoff_freq)) / sample_interval;
 
         Vector3 pe = (raw_pos + (k * filtered_prev)) / (1 + k);
-
-        raw_pos_prev = raw_pos;
         filtered_prev = pe;
 
         pe.x += offsetX;
         pe.y += offsetY;
+        pe.z += offsetZ;
 
-        Vector3 va = pa - pe;
-        Vector3 vb = pb - pe;
-        Vector3 vc = pc - pe;
+        Vector3 pes = pe - LL;
+        float L = Vector3.Dot(pes, Xs);
+        float R = (LR - LL).magnitude - L;
+        float B = Vector3.Dot(pes, Ys);
+        float T = (UL - LL).magnitude - B;
+        float distance = Vector3.Dot(pes, Zs);
 
-        float d = -Vector3.Dot(va, vn); // distance from eye to screen plane
-        if (d <= 0.001f) {
-            return; // prevent the projection from breaking if the user goes past the camera
-        }
+        float left = -L * near / distance;
+        float right = R * near / distance;
+        float bottom = -B * near / distance;
+        float top = T * near / distance;
 
-        float left = Vector3.Dot(vr, va) * (near / d);
-        float right = Vector3.Dot(vr, vb) * (near / d);
-        float bottom = Vector3.Dot(vu, va) * (near / d);
-        float top = Vector3.Dot(vu, vc) * (near / d);
+        Matrix4x4 frustum = Matrix4x4.Frustum(left, right, bottom, top, near, far);
+        cam.projectionMatrix = frustum;
 
-        // "Off Center Projection Matrix", taken from https://docs.unity3d.com/560/Documentation/ScriptReference/Camera-projectionMatrix.html
-        Matrix4x4 proj = Matrix4x4.zero;
-        proj[0, 0] = (2.0f * near) / (right - left);
-        proj[0, 2] = (right + left) / (right - left);
-        proj[1, 1] = (2.0f * near) / (top - bottom);
-        proj[1, 2] = (top + bottom) / (top - bottom);
-        proj[2, 2] = -(far + near) / (far - near);
-        proj[2, 3] = -(2.0f * far * near) / (far - near);
-        proj[3, 2] = -1.0f;
-
-        cam.projectionMatrix = proj; // sets the camera's projection matrix
-        cam.transform.position = pe; // update's the camera's position in world space with the head tracked location from the python program
-
-        cam.transform.rotation = Quaternion.identity; // we care not for camera rotation (at this point in time)
+        Matrix4x4 ViewMat = Matrix4x4.Translate(-pe) * RotMat;
+        cam.worldToCameraMatrix = ViewMat;
     }
 
     void Update()
     {
-        if (!calibrated) {
-            Calibration();
-            return;
-        }
-
-        ReceiveUDP();
-
-        // after getting the UDP packet and applying the projection, we look for keyboard inputs in order to calibrate the screen and adjust head data smoothing
+        // look for keyboard inputs in order to calibrate the screen and adjust head data smoothing
         float step = 0.001f;
         var keyboard = Keyboard.current;
         if (keyboard != null) {
@@ -188,6 +155,8 @@ public class OffAxisProjection : MonoBehaviour
                 if (keyboard.rightArrowKey.wasPressedThisFrame) offsetX += step;
                 if (keyboard.upArrowKey.wasPressedThisFrame) offsetY += step;
                 if (keyboard.downArrowKey.wasPressedThisFrame) offsetY -= step;
+                if (keyboard.leftBracketKey.wasPressedThisFrame) offsetZ -= step;
+                if (keyboard.rightBracketKey.wasPressedThisFrame) offsetZ += step;
                 if (keyboard.semicolonKey.wasPressedThisFrame) min_cutoff_freq = Mathf.Max(0.01f, min_cutoff_freq - step);
                 if (keyboard.quoteKey.wasPressedThisFrame) min_cutoff_freq += step;
                 if (keyboard.commaKey.wasPressedThisFrame) speed_coefficient = Mathf.Max(0.0f, speed_coefficient - step);
@@ -198,13 +167,68 @@ public class OffAxisProjection : MonoBehaviour
                 if (keyboard.rightArrowKey.isPressed) offsetX += step;
                 if (keyboard.upArrowKey.isPressed) offsetY += step;
                 if (keyboard.downArrowKey.isPressed) offsetY -= step;
+                if (keyboard.leftBracketKey.isPressed) offsetZ -= step;
+                if (keyboard.rightBracketKey.isPressed) offsetZ += step;
                 if (keyboard.semicolonKey.isPressed) min_cutoff_freq = Mathf.Max(0.01f, min_cutoff_freq - step);
                 if (keyboard.quoteKey.isPressed) min_cutoff_freq += step;
                 if (keyboard.commaKey.isPressed) speed_coefficient = Mathf.Max(0.0f, speed_coefficient - step);
                 if (keyboard.periodKey.isPressed) speed_coefficient = Mathf.Min(1.0f, speed_coefficient + step);
                 if (keyboard.equalsKey.wasPressedThisFrame) keyboard_mode_toggle = false;
             }
+            if (keyboard.enterKey.isPressed) { //finish calibration by hitting enter key
+
+                if(calibrated == false) {
+                    LL = new Vector3(LL.x - offsetX, LL.y - offsetY, LL.z - offsetZ);
+                    LR = new Vector3(LR.x - offsetX, LR.y - offsetY, LR.z - offsetZ);
+                    UL = new Vector3(UL.x - offsetX, UL.y - offsetY, UL.z - offsetZ);
+                    UR = new Vector3(UR.x - offsetX, UR.y - offsetY, UR.z - offsetZ);
+                    offsetX = 0;
+                    offsetY = 0;
+                    offsetZ = 0;
+                    cam.transform.position = new Vector3(0, 0, 0);
+                    cam.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                    cam.ResetProjectionMatrix();
+                    calibration_line.SetPosition(0, LL); // bottom left
+                    calibration_line.SetPosition(1, LR); // to bottom right
+                    calibration_line.SetPosition(2, UR); // to top right
+                    calibration_line.SetPosition(3, UL); // to top left
+                    calibration_line.SetPosition(4, LL); // back to bottom left
+                    calibration_line.enabled = false;
+
+                    // Directly from Dave Pape (https://www.evl.uic.edu/pape/caveproj/):
+                    Xs = LR - LL;
+                    Xs = (LR - LL) / Xs.magnitude;
+
+                    Ys = UL - LL;
+                    Ys = (UL - LL) / Ys.magnitude;
+
+                    Zs = -Vector3.Cross(Xs, Ys);
+
+                    RotMat = Matrix4x4.zero;
+                    RotMat[0, 0] = Xs.x;
+                    RotMat[0, 1] = Ys.x;
+                    RotMat[0, 2] = Zs.x;
+                    RotMat[1, 0] = Xs.y;
+                    RotMat[1, 1] = Ys.y;
+                    RotMat[1, 2] = Zs.y;
+                    RotMat[2, 0] = Xs.z;
+                    RotMat[2, 1] = Ys.z;
+                    RotMat[2, 2] = Zs.z;
+                    RotMat[3, 3] = 1.0f;
+
+                    RotMat = RotMat.inverse;
+
+                    calibrated = true;
+                }
+            }
         }
+
+        if (calibrated == false) {
+            Calibration();
+            return; //skip over data receiving until calibration is done
+        }
+
+        ReceiveUDP();
     }
 
     [System.Serializable]
